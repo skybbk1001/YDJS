ca79a01936a067988cc641a32438d9026c7499ef
skybbk1001 <renjianbingqi13@gmail.com>
2025-03-29 10:44:44 +0800

    update

--- yuedubao.js
+++ yuedubao.js
@@ -1,16 +1,12 @@
-// 常量定义
 const SOURCE_TIME = "1742387457506";
 const RSA_PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCOTz9KHYYoG+hBG7R2lN98CDxW4D9WCT60hYxeTxvGrBGZzLL2euNj9pIaY27/+6WX1a7yGrDad6fUm1hgyt6unlV+p7axneBbaesvqAUnaVQqcot2+P5SQgKJsP7QZjZYhVPRsQgkeaCbb1OYfMc3RO8L4AHqTUK3LmTwUB7fCQIDAQAB";
 const BASE_API_URL = "http://124.222.183.125";
 const AES_KEY_PREFIX = "yuedubao";
 const AES_KEY_SUFFIX = "y*y%h#123";
 const CHARS = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-// 缓存对象
 const memoCache = {};
 
-// 默认请求头 - 使用立即执行函数只计算一次
-const DEFAULT_HEADERS = (() => {
+const DEFAULT_HEADERS = (function() {
     return {
         version: "3.25.031915",
         androidId: String(Packages.java.util.UUID.randomUUID()).replace(/-/g, "").slice(0, 16),
@@ -20,60 +16,74 @@ const DEFAULT_HEADERS = (() => {
     };
 })();
 
-// Base64解码
 function b64d(text) {
     const { java } = this;
     if (!text) return "";
-    if (Array.isArray(text)) {
-        return text.filter(Boolean).map(x => java.base64Decode(String(x))).join(", ");
-    }
+    if (Array.isArray(text))
+        return text.filter(Boolean).map(function(x) {
+            return java.base64Decode(String(x));
+        }).join(", ");
     return java.base64Decode(String(text));
 }
 
-// 生成随机字符串
 function generateRandomString(length) {
-    if (length === undefined) length = 16;
-    return Array.from({ length }, () => 
-        CHARS.charAt(Math.floor(Math.random() * CHARS.length))
-    ).join('');
+    if (length === undefined) {
+        length = 16;
+    }
+    return Array.from({ length: length }, function() {
+        return CHARS.charAt(Math.floor(Math.random() * CHARS.length));
+    }).join('');
 }
 
-// 获取最终密钥 - 使用记忆化缓存
 function getFinalKey(randomStr) {
-    let cacheKey = `finalKey_${randomStr}`;
+    var cacheKey = "finalKey_" + randomStr;
     if (memoCache[cacheKey]) return memoCache[cacheKey];
-    
-    const { java } = this;
-    let combined = `${AES_KEY_PREFIX}${randomStr}${AES_KEY_SUFFIX}`;
-    let md5 = Packages.java.security.MessageDigest.getInstance("MD5");
+
+    var java = this.java;
+    var combined = AES_KEY_PREFIX + randomStr + AES_KEY_SUFFIX;
+    var md5 = Packages.java.security.MessageDigest.getInstance("MD5");
     md5.update(new Packages.java.lang.String(combined).getBytes("UTF-8"));
-    let hashBytes = md5.digest();
-    java.log("test1")
-    let hexHash = Packages.java.util.HexFormat.of().formatHex(hashBytes);
-    java.log("test2")
+    var hashBytes = md5.digest();
+    java.log("test1");
 
-    // 处理十六进制字符串
-    const evenChars = hexHash.split("").filter((_, i) => i % 2 === 0).join("").slice(4, 12);
-    const oddChars = hexHash.split("").filter((_, i) => i % 2 === 1).join("").slice(6, 14);
+    function bytesToHex(bytes) {
+        var hexChars = [];
+        for (var i = 0; i < bytes.length; i++) {
+            var hex = (bytes[i] & 0xFF).toString(16);
+            if (hex.length === 1) {
+                hex = "0" + hex;
+            }
+            hexChars.push(hex);
+        }
+        return hexChars.join("");
+    }
+    var hexHash = bytesToHex(hashBytes);
+    java.log("test2");
 
-    const result = evenChars + oddChars;
+    var evenChars = hexHash.split("").filter(function(_, i) {
+        return i % 2 === 0;
+    }).join("").slice(4, 12);
+    var oddChars = hexHash.split("").filter(function(_, i) {
+        return i % 2 === 1;
+    }).join("").slice(6, 14);
+    var result = evenChars + oddChars;
     memoCache[cacheKey] = result;
     return result;
 }
 
-// RSA加密
 function rsaEncrypt(text, publicKey) {
-    if (publicKey === undefined) publicKey = RSA_PUBLIC_KEY;
-    const { java } = this;
+    if (publicKey === undefined) {
+        publicKey = RSA_PUBLIC_KEY;
+    }
+    var java = this.java;
     return java.createAsymmetricCrypto("RSA/ECB/PKCS1Padding")
         .setPublicKey(java.base64DecodeToByteArray(publicKey))
         .encryptBase64(text);
 }
 
-// AES加密
 function aesEncrypt(aesKey, data) {
-    const { java } = this;
-    const encrypted = java.createSymmetricCrypto("AES/ECB/PKCS5Padding", aesKey, null)
+    var java = this.java;
+    var encrypted = java.createSymmetricCrypto("AES/ECB/PKCS5Padding", aesKey, null)
         .encryptBase64(data);
     return String(encrypted)
         .replace(/\+/g, "-")
@@ -81,64 +91,46 @@ function aesEncrypt(aesKey, data) {
         .replace(/=+$/, "");
 }
 
-// 获取请求头
 function getLocalServerHeadersMap(appToken) {
-    const headers = Object.assign({}, DEFAULT_HEADERS);
+    var headers = Object.assign({}, DEFAULT_HEADERS);
     headers.appToken = appToken || headers.appToken;
     return headers;
 }
 
-// 处理URL并准备请求
 function processUrl(url, method, body) {
-    const { java, cache } = this;
-    const randomStr = generateRandomString();
-    const aesKey = getFinalKey(randomStr);
-    
-    // 记录日志
+    var java = this.java;
+    var cache = this.cache;
+    var randomStr = generateRandomString();
+    var aesKey = this.getFinalKey(randomStr);
     java.log("rsak--" + randomStr);
     java.log("aesk--" + aesKey);
 
-    // 处理URL
-    const normalizedUrl = url.replace(/http:\/\/\d+(?=\/)/, BASE_API_URL);
-    const urlParts = normalizedUrl.split("?");
-    const baseUrl = urlParts[0];
-    
-    // 处理参数
-    const params = method === "POST" ? (body || "") : (urlParts[1] || "");
-    const encryptedParams = this.aesEncrypt(aesKey, params);
-
-    // 处理时间戳和token
-    const timestamp = Packages.java.lang.System.currentTimeMillis().toString();
-    const encryptedTime = this.aesEncrypt(aesKey, timestamp);
-    const token = cache.get("ydbao");
-    
-    // 准备请求头
-    const headers = getLocalServerHeadersMap(token);
+    var normalizedUrl = url.replace(/http:\/\/\d+(?=\/)/, BASE_API_URL);
+    var urlParts = normalizedUrl.split("?");
+    var baseUrl = urlParts[0];
+    var params = method === "POST" ? (body || "") : (urlParts[1] || "");
+    var encryptedParams = this.aesEncrypt(aesKey, params);
+    var timestamp = Packages.java.lang.System.currentTimeMillis().toString();
+    var encryptedTime = this.aesEncrypt(aesKey, timestamp);
+    var token = cache.get("ydbao");
+    var headers = getLocalServerHeadersMap(token);
     headers.encryptKey = this.rsaEncrypt(randomStr);
     headers.encryptType = "v1";
 
-    // 创建请求配置
-    const config = { method, headers };
-    
-    // 针对POST请求设置请求体
+    var config = { method: method, headers: headers };
     if (method === "POST") {
         config.body = encryptedParams.match(/.{1,76}/g).join("\n");
-        return `${baseUrl}?requestData=${encryptedTime},${JSON.stringify(config)}`;
+        return baseUrl + "?requestData=" + encryptedTime + "," + JSON.stringify(config);
     }
-    
-    // GET请求
-    return `${baseUrl}?requestData=${encryptedParams},${JSON.stringify(config)}`;
+    return baseUrl + "?requestData=" + encryptedParams + "," + JSON.stringify(config);
 }
 
-// 统一的HTTP请求函数
 function localServiceRequest(url, method, body) {
-    const { java } = this;
-    const requestUrl = this.processUrl(url, method, body);
-    const response = java.ajax(requestUrl);
-    return response;
+    var java = this.java;
+    var requestUrl = this.processUrl(url, method, body);
+    return java.ajax(requestUrl);
 }
 
-// 向后兼容的函数
 function localServiceAjax(url, method, body) {
     return this.localServiceRequest(url, method, body);
 }
@@ -147,13 +139,10 @@ function localServiceAsync(url, method, body) {
     return this.localServiceRequest(url, method, body);
 }
 
-// 批量请求
 function ajaxAll(urls) {
-    const { java } = this;
-    const processedUrls = urls.map(url => {
-        return url.indexOf(BASE_API_URL) === 0
-            ? this.processUrl(url, "GET")
-            : url;
-    });
+    var java = this.java;
+    var processedUrls = urls.map(function(url) {
+        return url.indexOf(BASE_API_URL) === 0 ? this.processUrl(url, "GET") : url;
+    }, this);
     return java.ajaxAll(processedUrls);
 }
