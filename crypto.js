a4074ad2504ab90356db5f6576d4fa53317a87c8
skybbk1001 <renjianbingqi13@gmail.com>
2025-04-01 01:58:40 +0800

    update

--- crypto.js
+++ crypto.js
@@ -1,9 +1,10 @@
-const expected = "U2JCl3g2/+94Wzk8oT6uORrH12qR5vM6gsFOKmkCxcA=";
-const errMsg = `⚠️ 书源安全警告
+let expected = "U2JCl3g2/+94Wzk8oT6uORrH12qR5vM6gsFOKmkCxcA=";
+let errMsg = `⚠️ 书源安全警告
 检测到非法篡改！本书源仅供免费分享，严禁二次售卖及商业用途
 请前往更新：https://skybook.pages.dev   <br>`;
+let comment = source.bookSourceComment;
 
-if (java.HMacBase64(a, 'HmacSHA256', "skybook") !== expected) {
+if (java.HMacBase64(comment, 'HmacSHA256', "skybook") !== expected) {
   java.longToast(errMsg);
   throw new Error(errMsg);
 }
