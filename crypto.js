7141907666cda1295e7917758c23c83397a9e12c
skybbk1001 <renjianbingqi13@gmail.com>
2025-04-01 12:51:22 +0800

    update

--- crypto.js
+++ crypto.js
@@ -1,10 +1,9 @@
-let expected = "U2JCl3g2/+94Wzk8oT6uORrH12qR5vM6gsFOKmkCxcA=";
-let errMsg = `⚠️ 书源安全警告
-检测到非法篡改！本书源仅供免费分享，严禁二次售卖及商业用途
-请前往更新：https://skybook.pages.dev   <br>`;
-let comment = source.bookSourceComment;
-
-if (java.HMacBase64(comment, 'HmacSHA256', "skybook") != expected) {
-  java.longToast(errMsg);
-  throw new Error(errMsg);
+const comment = source.bookSourceComment;
+const validHmac = "0L6dvwRP/3oyf27wu2ZgmFEGYNlQeg+mfON8m3hvQpo=";
+if (String(java.HMacBase64(comment, 'HmacSHA256', "skybook")) !== validHmac) {
+  const errorMsg = `⚠️书源安全警告
+检测到非法篡改！本书源仅供免费分享严禁二次售卖及商业用途
+请前往更新：https://skybook.pages.dev<br>`;
+  java.longToast(errorMsg);
+  throw new Error(errorMsg);
 }
